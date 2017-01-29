3f5f4a5a4922e193b41fc7882691b654dfb746f3
Johan Abildskov <joa@praqma.net>
2017-01-29 17:53:37 +0100

    This is obviously the commit-message

--- stat-issue.sh
+++ stat-issue.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #Could be added to get the path from the remote
-$number=$1
-echo $number
-curl -s https://api.github.com/repos/randomsort/connect-o-tron/issues/1
+repo="`git remote show origin | grep "Push" | xargs | sed 's/.*:\(.*\)/\1/')`"
+curl -s https://api.github.com/repos/$repo/issues/$1 | grep -q "\"Not Found\"" > /dev/null
+exit $((1 - $?))
+
